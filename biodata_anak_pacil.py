44446a9f65646d4bfa4072aa22d7fac408ad4df3
naufal.fikri11 <naufal.fikri11@ui.ac.id>
2022-09-08 00:40:01 +0700

    test

---
+++ biodata_anak_pacil.py
@@ -0,0 +1,18 @@
+# Mencetak judul dari program ini
+print("Biodata Anak Pacil 2021")
+
+# Mengambil input dari pengguna dan menyimpannya ke dalam variabel-variabel
+nama = input("Nama: ")
+alamat = input("Alamat: ")
+tempat_lahir = input("Tempat Lahir: ")
+tanggal_lahir = input("Tanggal Lahir: ")
+hobi = input("Hobi: ")
+
+# Mencetak isi dari variabel-variabel
+print()
+print("Biodataku:")
+print(nama)
+print(alamat)
+print(tempat_lahir)
+print(tanggal_lahir)
+print(hobi)
